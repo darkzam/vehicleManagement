d41e15a51eb14b28d9478c663fb0edd381e080ec
Zam <zamir.narvaez@correounivalle.edu.co>
2017-11-17 12:11:29 -0500

    0.0.4 = Bike Component: RegNumber Validation Finished

--- src/app/bikes/bike.component.ts
+++ src/app/bikes/bike.component.ts
@@ -20,6 +20,7 @@ export class BikeComponent {
 	type:string = ''; // one type between: road bike, mountain bike, BMX
 
 	msg:boolean = false;
+	status:string = '';
 
 	constructor(){}
 
@@ -29,55 +30,54 @@ export class BikeComponent {
 
 	validateRegNum(regNum:string):boolean{
 		
-	//return true if there's an error, false otherwise
-	
-		 if (regNum === '') {
-		 	return false;
-		 }
+		//return true if there's an error, false otherwise
+		console.log(regNum);
+		if (regNum === '') {
+			return false;
+		}
+
 		//checking first char of the string
 		let firstChar = regNum.charAt(0);
 
-		if(isNaN(+firstChar)){
-			//it's a valid char 'A''B'etc or invalid char ' ' etc 
-			//need to check for invalid chars
-			if(firstChar === ','){
-
-				return true;
-			}
-		}
-		else {
-			//firstchar is a number, or result of +"" or + " "
-			return true
-
+		if( (firstChar === firstChar.toUpperCase() && firstChar === firstChar.toLowerCase())|| (firstChar !== firstChar.toUpperCase())){
+			// it is true when string is a symbol
+			//OR it is tru when string is not a capital letter
+			this.status = "Registration Number: It must begin with a Capital Letter";
+			return true; 
 		}
 
-		//if the first char is a valid char, then it checks the rest of the string
+		//now checking for the required string size
 
-		if (isNaN(+regNum.slice(1))) {
-			//it is true when the rest of the string contains a char +"324B"
-			//or it is true when the string contains spaces in the middle +"234 34"
+		if ( regNum.length !== 10){
+			this.status = "Registration Number: Requires 10 Characters";
+			return true;
+		}
+
+		//if the first char is a valid char Capital Letter
+		// then it checks the rest of the string
+		
+		if ((regNum.slice(1).charAt(0) === " ")||(isNaN(+regNum.slice(1)))) {
+			// it is true when uf the first char is a space
+			//OR
+			//it is true when the string contains a char +"324B" +"A234" +"23f24"
+			//it is true when the string contains spaces in the middle +"234 34"
+			//it is false when the rest of the string is a number +"3435345" 	
+
+			this.status = "Registration Number: Capital Letter must be followed by a valid number";
 
 			return true;
 
 		}
-		else if (regNum.slice(1).charAt(0) === " ") {
-			//it is false when the rest of the string is a number +"3435345" 
-			//now we check for the +"  324234" case when the first char is a space
-			return true;
-		}
-​
 
 		return false;
 	}
 
 
-	validateRegNum2(){
-
-		//this.regNum = this.regNum.trim();
+	setErrorMsg(){
 
 		let param = this.regNum;
 
-		this.msg = this.validateRegNum(param);	
+		this.msg = this.validateRegNum(param.trim());	
 
 
 	}
