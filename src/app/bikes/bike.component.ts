85db476a96fd328b08e40b38e75fb47ab857270d
Zam <zamir.narvaez@correounivalle.edu.co>
2017-11-17 10:14:07 -0500

    bike validation finished but it needs to be tuned up a bit

--- src/app/bikes/bike.component.ts
+++ src/app/bikes/bike.component.ts
@@ -28,32 +28,44 @@ export class BikeComponent {
 	}
 
 	validateRegNum(regNum:string):boolean{
+		
+	//return true if there's an error, false otherwise
+	
+		 if (regNum === '') {
+		 	return false;
+		 }
+		//checking first char of the string
+		let firstChar = regNum.charAt(0);
 
-		let result = +regNum.charAt(0);
+		if(isNaN(+firstChar)){
+			//it's a valid char 'A''B'etc or invalid char ' ' etc 
+			//need to check for invalid chars
+			if(firstChar === ','){
 
-		if(typeof result === 'number'){
+				return true;
+			}
+		}
+		else {
+			//firstchar is a number, or result of +"" or + " "
+			return true
+
+		}
+
+		//if the first char is a valid char, then it checks the rest of the string
+
+		if (isNaN(+regNum.slice(1))) {
+			//it is true when the rest of the string contains a char +"324B"
+			//or it is true when the string contains spaces in the middle +"234 34"
 
 			return true;
+
 		}
-
-		if(isNaN(result)){
-
-			if(result === ''){
-
-				return true;
-			}
+		else if (regNum.slice(1).charAt(0) === " ") {
+			//it is false when the rest of the string is a number +"3435345" 
+			//now we check for the +"  324234" case when the first char is a space
+			return true;
 		}
-
-		if (isNaN(result)) { 
-
-			if (isNaN(Number(regNum.slice(1)))){
-
-				return true;
-
-			}
-		}
-
-		
+​
 
 		return false;
 	}
@@ -61,18 +73,12 @@ export class BikeComponent {
 
 	validateRegNum2(){
 
-		this.regNum = this.regNum.trim();
+		//this.regNum = this.regNum.trim();
 
 		let param = this.regNum;
 
-		if(param === ''){
+		this.msg = this.validateRegNum(param);	
 
-			this.msg = false;
-
-		} else {
-
-			this.msg = this.validateRegNum(param);	
-		}
 
 	}
 
